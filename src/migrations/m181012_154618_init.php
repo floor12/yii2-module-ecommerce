49f82717d483abcd078d3f4612298b830b89c508
floor12 <floor12@floor12.net>
2018-11-25 20:28:15 +0100

    Правка базовой миграции.

--- src/migrations/m181012_154618_init.php
+++ src/migrations/m181012_154618_init.php
@@ -33,7 +33,7 @@ class m181012_154618_init extends Migration
             'id' => $this->primaryKey(),
             'title' => $this->string()->notNull()->comment('Item title'),
             'subtitle' => $this->string()->null()->comment('Item subtitle'),
-            'description' => $this->string()->null()->comment('Item description'),
+            'description' => $this->text()->null()->comment('Item description'),
             'seo_description' => $this->text()->null()->comment('Description META'),
             'seo_title' => $this->string()->null()->comment('Page title'),
             'price' => $this->float()->null()->comment('Price'),
